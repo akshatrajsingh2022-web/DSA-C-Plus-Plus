061241beb86799709b8d05195f627da2e2eb7cea
Akshat raj singh <akshatrajsingh2022@gmail.com>
2026-07-21 23:09:02 +0530

    pointerIntro.cpp

---
+++ Module10_Pointers/pointerIntro.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=3;
+    int* p1=&x;
+    cout<<p1<<endl<<&x;
+    cout<<endl;
+
+    float y=3.53;
+    float* p2=&y;
+    cout<<p2<<endl<<&y;
+    cout<<endl;
+
+}
