238f2fab2e669b459e1d05071d5a66e1518c67fc
Akshat raj singh <akshatrajsingh2022@gmail.com>
2026-07-21 23:10:36 +0530

    revisitingVariables.cpp

---
+++ Module10_Pointers/revisitingVariables.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    //printing size of variable in bytes
+    cout<<sizeof(x)<<endl;
+    //printing address of variable
+    cout<<&x;
+}
